a3df639c1361f47cb0efd44ec3f110729bb0d9fd
adhithyakrishna <adhithyakrishna@skava.com>
2017-06-26 13:48:04 +0530

    compressed css and beautified js

--- public/js/script.js
+++ public/js/script.js
@@ -17,221 +17,222 @@ $(document).ready(function() {
 
         if ($(window).innerWidth() < 768) {
             customiseMobile();
-        }
-        else {
+        } else {
             customiseDesktop();
         }
 
-}
-
-function customiseMobile() {
-    var scrolledPos = $(window).scrollTop();
-    if (!(scrolledPos < 64)) {
-        $(".nav-holder").css('top', '-' + scrolledPos + 'px');
-    } else {
-        $(".nav-holder").css('top', 65 + 'px');
     }
-    // $(".nav-holder").height()
-    $(".content-frame").css('min-height', $(window).innerHeight() - 65 +'px');
-}
-function customiseDesktop() {
-    $(".nav-holder").css('top', 0 + 'px');
-}
-$(window).bind('resize scroll', function(event) {
-    setHeight();
-});
 
-// $(".nav-holder").hover(
-//     function() {
-//         $("body").toggleClass('nav-collapsed');
-//     }
-// );
+    function customiseMobile() {
+        var scrolledPos = $(window).scrollTop();
+        if (!(scrolledPos < 64)) {
+            $(".nav-holder").css('top', '-' + scrolledPos + 'px');
+        } else {
+            $(".nav-holder").css('top', 65 + 'px');
+        }
+        // $(".nav-holder").height()
+        $(".content-frame").css('min-height', $(window).innerHeight() - 65 + 'px');
+    }
 
-$(".addTaskBtn").on('click', function(event) {
-    var taskCont = $(".taskContainer .taskAdder").last().clone();
-    var lastCount = parseInt($(taskCont).attr("count"));
-    $(taskCont).attr("count", (lastCount + 1));
-    taskCont.find(".taskInput,.taskDescriptionInput").val("");
-    $(".taskContainer").append(taskCont);
-});
+    function customiseDesktop() {
+        $(".nav-holder").css('top', 0 + 'px');
+    }
+    $(window).bind('resize scroll', function(event) {
+        setHeight();
+    });
 
-$(".addTaskToDb").on('click', function(event) {
-    var isemptyFlag = false;
-    $.each($(".taskInput,.taskDescriptionInput"), function(index, val) {
-        if (!$(this).val().length) {
-            isemptyFlag = true;
+    // $(".nav-holder").hover(
+    //     function() {
+    //         $("body").toggleClass('nav-collapsed');
+    //     }
+    // );
+
+    $(".addTaskBtn").on('click', function(event) {
+        var taskCont = $(".taskContainer .taskAdder").last().clone();
+        var lastCount = parseInt($(taskCont).attr("count"));
+        $(taskCont).attr("count", (lastCount + 1));
+        taskCont.find(".taskInput,.taskDescriptionInput").val("");
+        $(".taskContainer").append(taskCont);
+    });
+
+    $(".addTaskToDb").on('click', function(event) {
+        var isemptyFlag = false;
+        $.each($(".taskInput,.taskDescriptionInput"), function(index, val) {
+            if (!$(this).val().length) {
+                isemptyFlag = true;
+            }
+        });
+        if (!isemptyFlag) {
+            $(this).html("adding...");
+        } else {
+            alert("Task / Task description cannot be empty");
+        }
+        var object = [];
+        var date = new Date();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+
+        var createdDate = date.getFullYear() + '-' +
+            (('' + month).length < 2 ? '0' : '') + month + '-' +
+            (('' + day).length < 2 ? '0' : '') + day;
+
+
+        var totContent = $(".taskContainer .taskAdder");
+        $.each(totContent, function(index, val) {
+            var content = {};
+            content.task = $(val).children(".taskInput").val();
+            content.taskDescription = $(val).children(".taskDescriptionInput").val();
+            content.createdAt = createdDate;
+            if (content.task && content.taskDescription) {
+                object.push(content);
+            }
+        });
+        if (object.length != 0) {
+            $.ajax({
+                url: "https://api.mlab.com/api/1/databases/testdbfortasklist/collections/users?apiKey=" + mlabapiKey,
+                type: "POST",
+                data: JSON.stringify(object),
+                contentType: "application/json"
+            }).done(function(msg) {
+                window.location.pathname = "/task";
+            });
+        }
+
+    });
+
+    $(".printJira").on('click', function(event) {
+        if ($(".myJiraDetails").length) {
+            $(".jiraDetailHolder").detach();
+        }
+        var selectDate = $(".strDate").val();
+        var caseValues = $(".taskDetails .taskIdentifier");
+        printforJira(selectDate, caseValues);
+    });
+
+    $(".listTasks").on('click', function(event) {
+        var pathtoGo = window.location.pathname;
+        window.location.href = pathtoGo;
+    });
+
+    $(".findIssues").on('click', function(event) {
+        if ($(".strDate").val().length == 0) {
+            alert("date shouldn't be empty");
+            event.preventDefault();
         }
     });
-    if (!isemptyFlag) {
-        $(this).html("adding...");
-    } else {
-        alert("Task / Task description cannot be empty");
-    }
-    var object = [];
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
+    $(".reopenTask").on('click', function(event) {
+        $(window).scrollTop(0);
+        $(".jiraDetailHolder").detach();
+        $(this).addClass('disabled');
+        var taskID = $(this).parent().parent().attr('id');
+        $(".taskIdentifier,.datePicker,.buttonContainer").hide();
+        $("#" + taskID).show();
+        $(".taskBoxHolder").empty();
 
-    var createdDate = date.getFullYear() + '-' +
-        (('' + month).length < 2 ? '0' : '') + month + '-' +
-        (('' + day).length < 2 ? '0' : '') + day;
+        $(".taskBoxHolder").html("<div class='commentBoxHolder' dataAttr='isReopened' id=task_" + taskID + "><input class='myTextHolder' type='textArea'></input><button class='upDateDetails'>update</button><button class='cancelInfo'>Cancel</button></div>");
+        $(".upDateDetails").on('click', function(event) {
+            var id = $(this).parent(".commentBoxHolder").attr('id').replace("task_", "");
+            var dataAttribute = "isReopened";
+            var comment = $(this).siblings('.myTextHolder').val();
+            updateInfo(id, dataAttribute, comment);
+        });
 
+        $(".cancelInfo").on('click', function(event) {
+            $(".commentBoxHolder").detach();
+            $(".reopenTask").removeClass('disabled');
+            $(".taskIdentifier,.datePicker,.buttonContainer").show();
+        });
 
-    var totContent = $(".taskContainer .taskAdder");
-    $.each(totContent, function(index, val) {
-        var content = {};
-        content.task = $(val).children(".taskInput").val();
-        content.taskDescription = $(val).children(".taskDescriptionInput").val();
-        content.createdAt = createdDate;
-        if (content.task && content.taskDescription) {
-            object.push(content);
-        }
     });
-    if (object.length != 0) {
-        $.ajax({
-            url: "https://api.mlab.com/api/1/databases/testdbfortasklist/collections/users?apiKey=" + mlabapiKey,
-            type: "POST",
-            data: JSON.stringify(object),
-            contentType: "application/json"
-        }).done(function(msg) {
-            window.location.pathname = "/task";
+
+    $(".taskCompleted,.taskRemoved").on('click', function(event) {
+        $(this).addClass('disabled');
+        var taskID = $(this).parent().parent().attr('id');
+        $(this).siblings().hide();
+        $(".taskIdentifier").hide();
+        $("#" + taskID).show();
+        $(".taskBoxHolder").empty();
+        var textBoxinfo = $(this).attr("dataAttr");
+        $(".taskBoxHolder").html("<div class='commentBoxHolder' dataAttr=" + textBoxinfo + " id=task_" + taskID + "><input class='myTextHolder' type='textArea'></input><button class='upDateDetails'>update</button><button class='cancelInfo'>Cancel</button></div>");
+        $(".upDateDetails").on('click', function(event) {
+            $(this).html("updating...")
+            var id = $(this).parent(".commentBoxHolder").attr('id').replace("task_", "");
+            var dataAttribute = $(this).parent(".commentBoxHolder").attr("dataattr");
+            var comment = $(this).siblings('.myTextHolder').val();
+            updateInfo(id, dataAttribute, comment);
+        });
+
+        $(".cancelInfo").on('click', function(event) {
+            $("button").removeClass('disabled');
+            $(".commentBoxHolder").detach();
+            $(".taskIdentifier,.taskCompleted,.taskRemoved").show();
+        });
+    });
+
+    function printforJira(date, Details) {
+        $(".contentHolder").append("<div class='jiraDetailHolder'><textArea class='myJiraDetails'></textArea><button class='copyBtn'>copy</button></div>");
+        if (date != "undefined" && date != "") {
+            var dateString = "*Task details of " + date + "* \n";
+        } else {
+            var dateString = "";
+        }
+        var taskDetailsHeader = "||Task Detail||Task Description|| \n ";
+        var taskInformation = dateString + taskDetailsHeader;
+        var removeHtmlregex = /(&nbsp;|<([^>]+)>)/ig;
+        $.each(Details, function(index, val) {
+
+            taskInformation = taskInformation + "|" + $(this).find(".task").html().replace(removeHtmlregex, "").replace(/(&gt;)(?:&nbsp;|<br>)+(\s?&lt;)/g, '$1$2') + "|" + $(this).find(".taskDescription").html().replace(/(&gt;)(?:&nbsp;|<br>)+(\s?&lt;)/g, '$1$2') + "| \n ";
+        });
+        console.log("the task information", taskInformation);
+        $(".myJiraDetails").val(taskInformation);
+
+        $(".copyBtn").on('click', function(event) {
+            $(".myJiraDetails").select();
+            document.execCommand('copy');
         });
     }
 
-});
+    function updateInfo(identifier, attribute, comment) {
+        var commentId = identifier.toString();
+        var commentAttr = attribute.toString();
+        var comment = comment.toString();
 
-$(".printJira").on('click', function(event) {
-    if ($(".myJiraDetails").length) {
-        $(".jiraDetailHolder").detach();
-    }
-    var selectDate = $(".strDate").val();
-    var caseValues = $(".taskDetails .taskIdentifier");
-    printforJira(selectDate, caseValues);
-});
+        var date = new Date();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
 
-$(".listTasks").on('click', function(event) {
-    var pathtoGo = window.location.pathname;
-    window.location.href = pathtoGo;
-});
+        var updatedDate = date.getFullYear() + '-' +
+            (('' + month).length < 2 ? '0' : '') + month + '-' +
+            (('' + day).length < 2 ? '0' : '') + day;
 
-$(".findIssues").on('click', function(event) {
-    if ($(".strDate").val().length == 0) {
-        alert("date shouldn't be empty");
-        event.preventDefault();
-    }
-}); $(".reopenTask").on('click', function(event) {
-    $(window).scrollTop(0);
-    $(".jiraDetailHolder").detach();
-    $(this).addClass('disabled');
-    var taskID = $(this).parent().parent().attr('id');
-    $(".taskIdentifier,.datePicker,.buttonContainer").hide();
-    $("#" + taskID).show();
-    $(".taskBoxHolder").empty();
+        // var date = new Date();
+        // var updatedDate = date.toISOString();
 
-    $(".taskBoxHolder").html("<div class='commentBoxHolder' dataAttr='isReopened' id=task_" + taskID + "><input class='myTextHolder' type='textArea'></input><button class='upDateDetails'>update</button><button class='cancelInfo'>Cancel</button></div>");
-    $(".upDateDetails").on('click', function(event) {
-        var id = $(this).parent(".commentBoxHolder").attr('id').replace("task_", "");
-        var dataAttribute = "isReopened";
-        var comment = $(this).siblings('.myTextHolder').val();
-        updateInfo(id, dataAttribute, comment);
-    });
+        var endurl = "https://api.mlab.com/api/1/databases/testdbfortasklist/collections/users?apiKey=" + mlabapiKey + "&q={_id:{$oid:%22" + commentId + "%22}}";
 
-    $(".cancelInfo").on('click', function(event) {
-        $(".commentBoxHolder").detach();
-        $(".reopenTask").removeClass('disabled');
-        $(".taskIdentifier,.datePicker,.buttonContainer").show();
-    });
-
-});
-
-$(".taskCompleted,.taskRemoved").on('click', function(event) {
-    $(this).addClass('disabled');
-    var taskID = $(this).parent().parent().attr('id');
-    $(this).siblings().hide();
-    $(".taskIdentifier").hide();
-    $("#" + taskID).show();
-    $(".taskBoxHolder").empty();
-    var textBoxinfo = $(this).attr("dataAttr");
-    $(".taskBoxHolder").html("<div class='commentBoxHolder' dataAttr=" + textBoxinfo + " id=task_" + taskID + "><input class='myTextHolder' type='textArea'></input><button class='upDateDetails'>update</button><button class='cancelInfo'>Cancel</button></div>");
-    $(".upDateDetails").on('click', function(event) {
-        $(this).html("updating...")
-        var id = $(this).parent(".commentBoxHolder").attr('id').replace("task_", "");
-        var dataAttribute = $(this).parent(".commentBoxHolder").attr("dataattr");
-        var comment = $(this).siblings('.myTextHolder').val();
-        updateInfo(id, dataAttribute, comment);
-    });
-
-    $(".cancelInfo").on('click', function(event) {
-        $("button").removeClass('disabled');
-        $(".commentBoxHolder").detach();
-        $(".taskIdentifier,.taskCompleted,.taskRemoved").show();
-    });
-});
-
-function printforJira(date, Details) {
-    $(".contentHolder").append("<div class='jiraDetailHolder'><textArea class='myJiraDetails'></textArea><button class='copyBtn'>copy</button></div>");
-    if (date != "undefined" && date != "") {
-        var dateString = "*Task details of " + date + "* \n";
-    } else {
-        var dateString = "";
-    }
-    var taskDetailsHeader = "||Task Detail||Task Description|| \n ";
-    var taskInformation = dateString + taskDetailsHeader;
-    var removeHtmlregex = /(&nbsp;|<([^>]+)>)/ig;
-    $.each(Details, function(index, val) {
-
-        taskInformation = taskInformation + "|" + $(this).find(".task").html().replace(removeHtmlregex, "").replace(/(&gt;)(?:&nbsp;|<br>)+(\s?&lt;)/g, '$1$2') + "|" + $(this).find(".taskDescription").html().replace(/(&gt;)(?:&nbsp;|<br>)+(\s?&lt;)/g, '$1$2') + "| \n ";
-    });
-    console.log("the task information", taskInformation);
-    $(".myJiraDetails").val(taskInformation);
-
-    $(".copyBtn").on('click', function(event) {
-        $(".myJiraDetails").select();
-        document.execCommand('copy');
-    });
-}
-
-function updateInfo(identifier, attribute, comment) {
-    var commentId = identifier.toString();
-    var commentAttr = attribute.toString();
-    var comment = comment.toString();
-
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-
-    var updatedDate = date.getFullYear() + '-' +
-        (('' + month).length < 2 ? '0' : '') + month + '-' +
-        (('' + day).length < 2 ? '0' : '') + day;
-
-    // var date = new Date();
-    // var updatedDate = date.toISOString();
-
-    var endurl = "https://api.mlab.com/api/1/databases/testdbfortasklist/collections/users?apiKey=" + mlabapiKey + "&q={_id:{$oid:%22" + commentId + "%22}}";
-
-    var jsonObj = {};
-    jsonObj[commentAttr] = true;
-    if (commentAttr == "isReopened") {
+        var jsonObj = {};
         jsonObj[commentAttr] = true;
-        jsonObj["isDeleted"] = false;
-        jsonObj["isCompleted"] = false;
-        jsonObj.updatedAt = updatedDate;
-        jsonObj.taskDescription = comment;
-    } else {
-        jsonObj["isReopened"] = false;
-        jsonObj.caseEndDetails = comment;
-        jsonObj.updatedAt = updatedDate;
+        if (commentAttr == "isReopened") {
+            jsonObj[commentAttr] = true;
+            jsonObj["isDeleted"] = false;
+            jsonObj["isCompleted"] = false;
+            jsonObj.updatedAt = updatedDate;
+            jsonObj.taskDescription = comment;
+        } else {
+            jsonObj["isReopened"] = false;
+            jsonObj.caseEndDetails = comment;
+            jsonObj.updatedAt = updatedDate;
+        }
+        $.ajax({
+            url: endurl,
+            data: JSON.stringify({
+                "$set": jsonObj
+            }),
+            type: "PUT",
+            contentType: "application/json"
+        }).done(function(msg) {
+            location.reload();
+        });;
     }
-    $.ajax({
-        url: endurl,
-        data: JSON.stringify({
-            "$set": jsonObj
-        }),
-        type: "PUT",
-        contentType: "application/json"
-    }).done(function(msg) {
-        location.reload();
-    });;
-}
     setHeight();
 });
